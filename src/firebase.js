597edbb602569a5068e820e4b9f46577aeef71c0
lee <letmalikdothat@gmail.com>
2021-11-04 18:02:05 -0400

    modified

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBD7ddyAfxJXQzzomCzWclhryz6gZMsiok",
+    authDomain: "nwitter-4b9f6.firebaseapp.com",
+    projectId: "nwitter-4b9f6",
+    storageBucket: "nwitter-4b9f6.appspot.com",
+    messagingSenderId: "541769675572",
+    appId: "1:541769675572:web:f566a378cb4594f8eeab61"
+  };
+  
+  // Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
